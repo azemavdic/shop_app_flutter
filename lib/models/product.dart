ba5bd2e4e88f71968899ed3baffba32623b19d67
unknown <asko2212@hotmail.com>
2021-06-16 07:36:05 +0200

    product model

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final String description;
+  final double price;
+  bool isFavorite;
+
+  Product({
+    this.id,
+    this.title,
+    this.imageUrl,
+    this.description,
+    this.price,
+    this.isFavorite,
+  });
+}
