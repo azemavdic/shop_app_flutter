9501074a6d84be6f18e413dead9ec1b1fd9fb877
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-06-21 02:03:06 +0200

    expanding orders

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,25 +1,73 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../models/order_item.dart' as ord;
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   const OrderItem({Key key, this.order}) : super(key: key);
 
   @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
-      child: ListTile(
-        title: Text('${order.amount} KM'),
-        subtitle: Text(
-          DateFormat('dd.MM.yyyy | hh:mm').format(order.dateTime),
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: () {},
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('${widget.order.amount} KM'),
+            subtitle: Text(
+              DateFormat('dd.MM.yyyy | hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height:
+                  min(widget.order.products.length * 20 + 10, 100).toDouble(),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${prod.quantity} x ${prod.price} KM',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
+        ],
       ),
     );
   }
